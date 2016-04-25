8bd45cf63d90ef9a6161f84f38a4ae7ad854d410
jenshadlich <jenshadlich@googlemail.com>
2016-04-25 09:07:13 +0200

    add simple junit test

---
+++ HelloWorldMaven/src/test/java/java101/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package java101;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void test() {
+        assertNotEquals("foo", "bar");
+    }
+}
