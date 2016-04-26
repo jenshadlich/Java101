7dea2e8d96b7c4ab3387a4f457f2e74bdffc8182
Jens Hadlich <jenshadlich@googlemail.com>
2016-04-26 18:22:18 +0200

    don't use deprecated methods

--- HelloWorldMaven/src/main/java/java101/HelloWorldFromFile.java
+++ HelloWorldMaven/src/main/java/java101/HelloWorldFromFile.java
@@ -4,13 +4,14 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class HelloWorldFromFile {
 
     public static void main(String... args) {
         File file = new File("helloworld.txt");
         try {
-            String fileContents = FileUtils.readFileToString(file);
+            String fileContents = FileUtils.readFileToString(file, Charset.forName("UTF-8"));
             System.out.println(fileContents);
         } catch (IOException e) {
             throw new RuntimeException(e);
